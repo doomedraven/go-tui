b8f35fd9dbc2df71942abfc38603023aebf02a5f
Serge Smertin <259697+nfx@users.noreply.github.com>
2026-09-18 20:30:54 +0200

    added ASCII truncation

--- view.go
+++ view.go
@@ -97,6 +97,49 @@ func width(chunk []byte) int {
 	return w
 }
 
+func truncateASCII(chunk string, maxLen int) (out string) {
+	defer func() {
+		if r := recover(); r != nil {
+			out = "…" // this is quite a hack
+		}
+	}()
+	lo, hi, w, m := 0, len(chunk), 0, 0
+	var escape bool
+	for lo < hi {
+		if escape && isEscapeEnd(chunk[lo]) {
+			escape = false
+			if chunk[lo] == 'm' {
+				if chunk[lo-1] == '0' {
+					m-- // Select Graphics Rendition (SGR) end
+				} else {
+					m++ // SGR start
+				}
+			}
+		} else if isEscapeStart(chunk[lo]) {
+			escape = true
+			w--
+		}
+		if !escape {
+			if w >= (maxLen - 1) {
+				break
+			}
+			w++
+		}
+		lo++
+	}
+	if lo == hi {
+		return chunk
+	}
+	raw := []rune(chunk)
+	raw = append(raw[:lo], '…')
+	for range m {
+		// reset every SGR
+		raw = append(raw, '\x1b', '[', '0', 'm')
+	}
+	out = string(raw)
+	return
+}
+
 func (v *view) padded(chunk []byte, lo, mid int) (int, int) {
 	pos, line := lo, []byte{}
 	for pos < mid {
--- view_test.go
+++ view_test.go
@@ -84,3 +84,29 @@ func TestViewLinkedList(t *testing.T) {
 	v.WriteTo(&buf)
 	assert.Equal(t, "a\nb\nc\nd\n", buf.String())
 }
+
+func TestTruncateASCII(t *testing.T) {
+	for _, tt := range []struct {
+		in  string
+		out string
+	}{
+		{"12345\x1b[1;31m6\x1b[0m789", "123…"},
+		{"\x1b[1;31m12\x1b[0m\x1b[1;32m345\x1b[0m", "\x1b[1;31m12\x1b[0m\x1b[1;32m3…\x1b[0m"},
+		{"1\x1b[1;31m2\x1b[0m345", "1\x1b[1;31m2\x1b[0m3…"},
+		{"1\x1b[1;31m23\x1b[0m45", "1\x1b[1;31m23\x1b[0m…"},
+		{"1\x1b[1;31m234\x1b[0m5", "1\x1b[1;31m23…\x1b[0m"},
+		{"1\x1b[1;31m2\x1b[1;32m3\x1b[0m45\x1b[0m6", "1\x1b[1;31m2\x1b[1;32m3\x1b[0m…\x1b[0m"},
+		{"1\x1b[1;31m2\x1b[1;32m34\x1b[0m5\x1b[0m6", "1\x1b[1;31m2\x1b[1;32m3…\x1b[0m\x1b[0m"},
+		{"", ""},
+		{"a", "a"},
+		{"ab", "ab"},
+		{"abc", "abc"},
+		{"abcd", "abc…"},
+		{"abcdef", "abc…"},
+		{"abcde", "abc…"},
+	} {
+		t.Run(fmt.Sprint(tt), func(t *testing.T) {
+			assert.Equal(t, tt.out, string(truncateASCII(tt.in, 4)))
+		})
+	}
+}
