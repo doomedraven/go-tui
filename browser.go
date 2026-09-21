a79fe95d8c6c02fdca2a1f7ca027faa6d8d02885
Serge Smertin <259697+nfx@users.noreply.github.com>
2026-09-21 13:15:32 +0200

    fixed url escaping for browser opens

--- browser.go
+++ browser.go
@@ -5,21 +5,27 @@ package tui
 
 import (
 	"fmt"
+	"net/url"
 	"os/exec"
 	"runtime"
 )
 
 // Browserf opens the specified URL in the default browser
-func Browserf(url string, args ...any) error {
+func Browserf(addr string, args ...any) error {
 	var cmd *exec.Cmd
-	url = fmt.Sprintf(url, args...)
+	for i := range args {
+		if s, ok := args[i].(string); ok {
+			args[i] = url.QueryEscape(s)
+		}
+	}
+	addr = fmt.Sprintf(addr, args...)
 	switch runtime.GOOS {
 	case "linux":
-		cmd = exec.Command("xdg-open", url)
+		cmd = exec.Command("xdg-open", addr)
 	case "windows":
-		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", addr)
 	case "darwin": // macOS
-		cmd = exec.Command("open", url)
+		cmd = exec.Command("open", addr)
 	default:
 		return fmt.Errorf("unsupported platform: %s", runtime.GOOS)
 	}
